35d541c35d617366566ed3d5e686874b7ca62e7d
Jagadeeshwar Goshika <jagadishwar2010@gmail.com>
2023-02-23 19:07:51 +0530

    Signup form

--- chat/urls.py
+++ chat/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.front_page, name='front_page')
+    path('', views.front_page, name='front_page'),
+    path('signup/', views.sign_up, name='sign_up')
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
+from django.contrib.auth import login
+from django.http import HttpResponse
+
 
 # Create your views here.
-
-
 def front_page(request):
     return render(request, 'chat/front_page.html')
 
+
+def sign_up(request):
+    if request.method == 'POST':
+        # return HttpResponse(request)
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('front_page')
+
+    else:
+        form = SignUpForm
+
+    return render(request, 'chat/signup.html', {'form': form})
